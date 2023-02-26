664bd79526da47fedfcd356d6d6f6c20eb14e36b
devdel2 <ddeleon2@uccs.edu>
2023-02-25 22:21:33 -0700

    created react app and started MVP

---
+++ apex-team-generator/src/App.js
@@ -0,0 +1,55 @@
+//IMPORTS AND REQUIRES
+import { useState } from 'react';
+import './App.css';
+
+//INSTANCE VARS
+const title = "Apex Team Generator";
+const numLegends = 23;
+const [solo, duo, trio] = [1,2,3];
+let randTeam = [];
+
+// COMPONENTS //
+//TITLE
+function Title() {
+  return <div className="title bg-red">{title}</div>
+}
+
+//ROULETTE SQUARE
+function RSquare({ legend }) {
+  return <div className="square">{legend}</div>
+}
+
+//ROUTLETTE
+function Roulette() {
+  const [squares, setSquares] = useState(Array(3).fill(null));
+  return (
+    <>
+      <div className="title">
+        <div>Generate Your Team</div>
+        <div className="roulette-row">
+          <RSquare legend={squares[0]} />
+          <RSquare legend={squares[0]} />
+          <RSquare legend={squares[0]} />
+          <SpinButton />
+        </div>
+      </div>
+    </>
+  )
+}
+
+//SPIN BUTTON
+function SpinButton({ randTeam }) {
+  return <button className="centered" onClick={randTeam}>Spin!</button>
+}
+
+//RENDER COMPONENTS IN APP
+function App() {
+  return (
+    <>
+      <Title />
+      <Roulette />
+    </>
+  );
+}
+
+export default App;
