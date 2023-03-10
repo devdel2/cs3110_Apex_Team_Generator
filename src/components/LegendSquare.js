27ae98623ba425ce2b93e6cd5ee8cf97db056b8b
devdel2 <ddeleon2@uccs.edu>
2023-03-10 12:16:08 -0700

    added cloudinary, need to hide cloud name, and create array for dynamic access

--- src/components/LegendSquare.js
+++ src/components/LegendSquare.js
@@ -1,6 +1,22 @@
+import React from 'react';
+import { AdvancedImage } from '@cloudinary/react'
+import { Cloudinary } from '@cloudinary/url-gen';
+
+const cld = new Cloudinary({
+  cloud: {
+    cloudName : 'dyazgbdqc'
+  }
+})
+
+const ash = cld.image('Legends/Ash/ash');
+
 //ROULETTE SQUARE
 function LegendSquare({ legend }) {
-    return <div className="square v-spacer">{legend}</div>
+    return (
+      <div className="square v-spacer">
+        <AdvancedImage className="legend-pic" cldImg={ash}/>
+      </div>
+    )
   } 
 
 export default LegendSquare
