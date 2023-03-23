322d0123019339484c9c215cbbd791ea7169d515
devdel2 <ddeleon2@uccs.edu>
2023-03-23 09:58:47 -0600

    added legend data to front end from JSON

--- src/components/LegendSquare.js
+++ src/components/LegendSquare.js
@@ -2,6 +2,8 @@ import React from 'react';
 import LegendPhotoData from './LegendPhotoData';
 import { AdvancedImage } from '@cloudinary/react'
 import { Cloudinary } from '@cloudinary/url-gen';
+//import the data from the JSON file
+import LegendData from './data/legend-data.json';
 
 const cld = new Cloudinary({
   cloud: {
@@ -9,18 +11,33 @@ const cld = new Cloudinary({
   }
 })
 
-const legendImg = (value) => {
+const getLegendImg = (value) => {
   return LegendPhotoData[value];
 }
 
+const getLegendData = (value) => {
+  return LegendData[value];
+}
+
+
 //ROULETTE SQUARE
 function LegendSquare({ legend }) {
-    legendImg(legend)
-    return (
-      <div className="square v-spacer">
-        <AdvancedImage className="legend-pic" cldImg={cld.image(legendImg(legend))}/>
-      </div>
+    //getLegendImg(legend)
+    const currLegend = getLegendData(legend)
+    return(
+      <>
+        <div className="column">         
+          <div className="square v-spacer">
+            <AdvancedImage className="legend-pic" cldImg={cld.image(getLegendImg(legend))}/>
+          </div>
+          <h1 className="title">
+            {currLegend.legendName}
+          </h1>
+        </div>
+      </>
     )
+     
+    
   } 
 
 export default LegendSquare
