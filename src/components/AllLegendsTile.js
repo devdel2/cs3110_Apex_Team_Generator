29a91a568056c98ece9daadf92dc87a2bdfb0519
devdel2 <ddeleon2@uccs.edu>
2023-04-09 03:12:57 -0600

    added link functionality to the all legends tile

--- src/components/AllLegendsTile.js
+++ src/components/AllLegendsTile.js
@@ -3,9 +3,9 @@ const AllLegendsTile = () => {
     const title = "All Legends"
     return (
         <>
-            <div className="rec-tile">
-                <h1 className="title">{title}</h1>
-            </div>
+            <a href="/legends" className="rec-tile">
+                        <h1 className="title">{title}</h1>
+            </a>
         </>
     )
 }
